738d04e16a200dc8324a2a68db71821f448d2367
Oleksandr Manenko <oleksandr@manenko.com>
2017-07-07 21:01:34 +0300

    Finish implementation of the first version

--- README.rst
+++ README.rst
@@ -1,6 +1,107 @@
-**Work in Progress**
+=============
+boot-download
+=============
 
 |clojars|  |license|
+
+`Boot`_ task to download a file from a given url.
+
+-----
+Tasks
+-----
+
+.. code-block:: clojure
+
+   (download-file [u url URL str])
+
+Downloads a single file from the given url and adds it to the fileset
+as an asset.
+                
+---------
+Functions
+---------
+
+~~~~~~~~~~~~~~~~~~~~~~~~
+get-all-downloaded-files
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. code-block:: clojure
+
+   (get-all-downloaded-files [fileset])
+
+Gets a seq of files (as :code:`TmpFile` objects) downloaded by the
+:code:`download-file` task.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+get-all-files-downloaded-from
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. code-block:: clojure
+
+   (get-all-files-downloaded-from [fileset url])
+
+Gets a seq of files (as :code:`TmpFile` objects) downloaded from the
+given url by the :code:`download-file` task.
+
+It could happen that the :code:`fileset` has a few files downloaded
+from the same url. That's why this function returns a seq instead of a
+single object (or :code:`nil`). The sequence will be empty, if there
+is no files downloaded from the given url.
+
+~~~~~~~~~~~~~~~~
+get-download-url
+~~~~~~~~~~~~~~~~
+
+.. code-block:: clojure
+
+   (get-download-url [tmpfile])
+
+Gets a url the given :code:`TmpFile` was downloaded from.
+
+
+-----
+Usage
+-----
+
+~~~~~~~~~~
+build.boot
+~~~~~~~~~~
+
+TBD
+
+~~~~
+REPL
+~~~~
+
+~~~~~~~
+Console
+~~~~~~~
+
+While the task is not designed to be usable from a console you still can invoke it:
+
+.. code-block:: text
+
+    boot download-file --url https://raw.githubusercontent.com/manenko/boot-download/master/README.rst
+
+
+----
+TODO
+----
+
+* Add an optional parameter :code:`--output-path` to the
+  :code:`download-file` task. If the output path is :code:`nil` then
+  the task will try to get the name of the file from the url and
+  download it to the root.
+                
+-------
+License
+-------
+
+Copyright © 2017 Oleksandr Manenko.
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
+
+.. _Boot: https://github.com/boot-clj/boot
 
 .. |clojars| image:: https://img.shields.io/clojars/v/manenko/boot-download.svg
     :alt: Clojars
--- src/manenko/boot_download.clj
+++ src/manenko/boot_download.clj
@@ -6,39 +6,64 @@
   (:import java.net.URI
            java.nio.file.Paths))
 
-(defn ^:private add-file-to-meta
-  [meta url file]
-  (assoc-in meta [:metadata ::downloaded-files url] file))
-
 
 (defn ^:private get-path
   [url]
   (.getPath (URI. url)))
 
 
-(defn get-file-name
+(defn ^:private get-file-name
   [url]
   (let [path (Paths/get (get-path url)
                         (make-array String 0))]
     (str (.getFileName path))))
 
 
-(boot/deftask download-file
-  "Downloads a single file from a remote HTTP(S) server.
+(defn get-download-url
+  "Gets a url the given `TmpFile` was downloaded from."
+  [tmpfile]
+  (get tmpfile ::downloaded-from))
 
-It puts a path to the downloaded file to a fileset's metadata
-under the :manenko.boot-download/downloaded-files key. The value of this key is a
-map of {url, path}, where both url and path are strings."
+
+(defn get-all-downloaded-files
+  "Gets a seq of files (as `TmpFile` objects) downloaded by the `download-file` task."
+  [fileset]
+  (let [all-files (boot/ls fileset)]
+    (boot/by-meta [#(contains? % ::downloaded-from)] all-files)))
+
+
+(defn get-all-files-downloaded-from
+  "Gets a seq of files (as `TmpFile` objects) downloaded from the
+  given url by the `download-file` task.
+
+  It could happen that `fileset` has a few files downloaded from the
+  same url. That's why this function returns a seq instead of a single
+  object (or `nil`). The sequence will be empty, if there is no files
+  downloaded from the given url."
+  [fileset url]
+  (let [all-files (boot/ls fileset)]
+    (boot/by-meta [#(= (get % ::downloaded-from) url)])))
+
+
+(boot/deftask download-file
+  "Downloads a single file from the given url.
+
+The task downloads the file and adds it to the fileset as an asset.
+
+Use the `get-all-downloaded-files` to get all downloaded files as a seq of
+`TmpFile` objects.
+
+Use the `get-all-files-downloaded-from` to get all files downloaded from the
+given url as a seq of `TmpFile` objects.
+
+Use the `get-download-url` to get a url the given `TmpFile` was downloaded from."
   [u url URL str "The location of the remote file."]
-  (let [tmp (boot/cache-dir! ::tools)]
+  (let [tmp (boot/tmp-dir!)]
     (boot/with-pre-wrap fileset
       (let [file-name (get-file-name url)
             file      (io/file tmp file-name)]        
         (with-open [in  (io/input-stream url)
                     out (io/output-stream file)]
           (io/copy in out))
-        (let [old-meta    (meta fileset)
-              new-meta    (add-file-to-meta old-meta url (str file))
-              new-fileset (with-meta fileset new-meta)]
-          (boot/commit! new-fileset))))))
-
+        (let [fileset' (boot/add-asset fileset tmp :meta {::downloaded-from url})]
+          (boot/commit! fileset'))))))
